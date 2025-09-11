049b3b68c7e56423911fbcc46a1d10f520de29f4
Lorena Rios <lorenadejesusrios@gmail.com>
2025-09-11 13:55:32 -0300

    Refactor code structure for improved readability and maintainability

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -33,3 +33,53 @@ var swiper = new Swiper(".mySwiper", {
       menu.classList.toggle('hidden');
     });
   });
+
+
+  // FUNÇÃO PARA ABRIR E FECHAR O MODAL
+  function openModal(titulo, zap = false) {
+    var modal = document.querySelector(".modal");
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+    modal.querySelector('[name="interesse"]').value = titulo;
+    modal.querySelector(".modal-title").innerHTML = titulo;
+    var form = modal.querySelector("form");
+    if (zap) {
+      form.setAttribute("data-zap", "sim");
+    } else {
+      form.removeAttribute("data-zap");
+    }
+  }
+  function closeModal() {
+    var modal = document.querySelector(".modal");
+    modal.classList.add("hidden");
+  }
+
+
+  // MODAL DE IMAGEM
+  function openImageModal(src) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+    modalImage.src = src;
+    modal.classList.remove("hidden");
+    document.body.style.overflow = "hidden"; // trava scroll
+  }
+
+  function closeImageModal() {
+    const modal = document.getElementById("imageModal");
+    modal.classList.add("hidden");
+    document.body.style.overflow = ""; // libera scroll
+  }
+
+  // Fechar clicando fora da imagem
+  document.getElementById("imageModal").addEventListener("click", function (e) {
+    if (e.target === this) {
+      closeImageModal();
+    }
+  });
+
+  // Fechar com ESC
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      closeImageModal();
+    }
+  });
